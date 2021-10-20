4ba81a45e180745be26329c32968127384b481a2
Jitender Kumar <Jitender1.kumar@commdel.local>
2021-10-20 09:58:47 +0000

    Add project files.

---
+++ JWTDemo/Controllers/SecurityController.cs
@@ -0,0 +1,37 @@
+﻿
+using JWTDemo.Model;
+using JWTDemo.Security.JWTToken;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace JWTDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SecurityController : ControllerBase
+    {
+        private readonly IJWTAuth jwtAuth;
+
+        public SecurityController(IJWTAuth jwtAuth)
+        {
+            this.jwtAuth = jwtAuth;
+        }
+
+        [HttpPost("authentication")]
+        public IActionResult Authentication([FromBody] UserCredential userCredential)
+        {
+
+
+            var token = jwtAuth.Authentication(userCredential.Username, userCredential.Password);
+            if (token == null)
+                return Unauthorized();
+            return Ok(token);
+        }
+
+    }
+}
---
+++ JWTDemo/Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace JWTDemo.Controllers
+{
+
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
